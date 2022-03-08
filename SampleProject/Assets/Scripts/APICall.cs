ec8b56a9351adb416b43009f65f0f7174f1ab4c2
Bert <bert.vanitterbeeck@student.ap.be>
2022-03-08 08:37:30 +0100

    added text

--- SampleProject/Assets/Scripts/APICall.cs
+++ SampleProject/Assets/Scripts/APICall.cs
@@ -23,31 +23,24 @@ namespace Assets.APIScripts
             Debug.Log(cityInput);
         }
         
-        // Where to send our request
 
-        void Update()
+        public void Update()
         {
             targetUrl = DEFAULT_URL;
             CityName.text = cityInput;
         }
-
-        // Keep track of what we got back
         private string recentData = "";
 
         private IEnumerator RequestRoutine(string url, Action<string> callback = null)
         {
-            // Using the static constructor
             var request = UnityWebRequest.Get(url);
 
-            // Wait for the response and then get our data
+
             yield return request.SendWebRequest();
             var data = request.downloadHandler.text;
 
-            // This isn't required, but I prefer to pass in a callback so that I can
-            // act on the response data outside of this function
             callback?.Invoke(data);
         }
-        // Callback to act on our response data
         private void ResponseCallback(string data)
         {
             Debug.Log(data);

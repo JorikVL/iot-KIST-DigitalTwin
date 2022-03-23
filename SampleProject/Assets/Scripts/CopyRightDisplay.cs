2f55c4ee778f216ee4551f9d50f599b386e029dd
JorikVL <jorik.vanlooke@student.ap.be>
2022-03-23 15:03:27 +0100

    updated location of copyright

--- SampleProject/Assets/Scripts/CopyRightDisplay.cs
+++ SampleProject/Assets/Scripts/CopyRightDisplay.cs
@@ -1,18 +1,18 @@
-﻿//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
-//using Microsoft.Maps.Unity;
+﻿//using system.collections;
+//using system.collections.generic;
+//using unityengine;
+//using microsoft.maps.unity;
 
-//public class CopyRightDisplay : MonoBehaviour
+//public class copyrightdisplay : monobehaviour
 //{
-//    public MapRenderer copyrightinfo;
-//    public Text ToprintCopyRight;
+//    public maprenderer copyrightinfo;
+//    public text toprintcopyright;
 
 
-//    if (ToprintCopyRight == null)
+//    if (toprintcopyright == null)
 //	{
-//        ToprintCopyRight= this.Text
-//    }
+//        toprintcopyright= this.text
+//}
 
     
 

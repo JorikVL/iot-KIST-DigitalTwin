29a4a4ab841849d7decedb9f9ef8d30f10b88ec2
SiemenSlabbinck <siemen.slabbinck@student.ap.be>
2022-04-01 09:43:47 +0300

    Add JSONReader script
    JSON file to multiple sensor objects

---
+++ SampleProject/Assets/Scripts/JSONReader.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JSONReader : MonoBehaviour
+{
+    public TextAsset textJSON;
+
+    [System.Serializable]
+    public class Sensor
+    {
+        public string position;
+        public float value;
+    }
+
+    [System.Serializable]
+    public class SensorList
+    {
+        public Sensor[] sensor;
+    }
+
+    public SensorList mySensorList = new SensorList();
+
+    void Start()
+    {
+        mySensorList = JsonUtility.FromJson<SensorList>(textJSON.text);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

79e74742d37583894ce8e3ac23d1de9603cd9d22
SiemenSlabbinck <siemen.slabbinck@student.ap.be>
2022-04-08 14:29:34 +0300

    Display data and changes to UI

--- SampleProject/Assets/Scripts/APICall.cs
+++ SampleProject/Assets/Scripts/APICall.cs
@@ -11,46 +11,23 @@ namespace Assets.APIScripts
     public class APICall : MonoBehaviour
     {
         private const string API_KEY = "73cb75a2a3ef8b58e77c3581ec7b4726";
-        public static string cityInput = "Zanzibar";
-        private static string DEFAULT_URL = $"api.openweathermap.org/data/2.5/weather?q={cityInput}&APPID={API_KEY}&units=metric";
-        public Text DataDisplay;
-        public Text CityName;
+        private static string DEFAULT_URL = "http://192.168.100.134:1880/Data1";
         string targetUrl = DEFAULT_URL;
         private string recentData = "";
         private JSONReader jsonReader;    
-
-        public void ReadStringInput(string s)
-        {
-            cityInput = s;
-            DEFAULT_URL = $"api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&APPID=" + API_KEY + "&units=metric";
-            Debug.Log(cityInput);
-        }
-        
-
-        public void Update()
-        {
-            targetUrl = DEFAULT_URL;
-            CityName.text = cityInput;
-        }
 
         private IEnumerator RequestRoutine(string url, Action<string> callback = null)
         {
             var request = UnityWebRequest.Get(url);
             yield return request.SendWebRequest();
             var data = request.downloadHandler.text;
-            if (data != null)
-            {
-                callback?.Invoke(data);
-            } else {
-                Debug.Log("API call returned null");
-            }
+            callback?.Invoke(data);
         }
 
         private void ResponseCallback(string data)
         {
             jsonReader = this.GetComponent<JSONReader>();
             jsonReader.AddSensor(data);
-            recentData = data;
         }
 
         public void ApiCall()
@@ -58,27 +35,9 @@ namespace Assets.APIScripts
             this.StartCoroutine(this.RequestRoutine(targetUrl, this.ResponseCallback));
         }
 
-        public void Awake()
+        public void Start()
         {
-            GetData();
-        }
-        
-        public void AreaOne()
-        {
-            cityInput = "Antwerpen";
-            DEFAULT_URL = "http://159.223.210.10:1880/Area1";
-        }
-
-        public void AreaTwo()
-        {
-            cityInput = "CapeTown";
-            DEFAULT_URL = "http://159.223.210.10:1880/Area2";
-        }
-
-        public void AreaTree()
-        {
-            cityInput = "Zanzibar";
-            DEFAULT_URL = "http://192.168.100.134:1880/Data1";
+            InvokeRepeating("GetData", 0, 300);
         }
 
         public void GetData(){
